c4659dc963c7916f86c8adf8287eade2837195c1
thepmclub <36820376+thepmclub@users.noreply.github.com>
2018-02-25 17:05:34 +0000

    Update openvpnsetup.sh

--- openvpnsetup.sh
+++ openvpnsetup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-wget http://vpnsetup.the-pm.club/openvpn/enigma2-plugin-extensions-vpnchanger_1.1.0_all.ipk -O /tmp/enigma2-plugin-extensions-vpnchanger_1.1.0_all.ipk
+wget https://github.com/thepmclub/VPNSetup/blob/master/enigma2-plugin-extensions-vpnchanger_1.1.0_all.ipk?raw=true-O /tmp/enigma2-plugin-extensions-vpnchanger_1.1.0_all.ipk
 opkg install /tmp/enigma2-plugin-extensions-vpnchanger_1.1.0_all.ipk
 mv /usr/lib/enigma2/python/Plugins/Extensions/VpnChanger/plugin.py /usr/lib/enigma2/python/Plugins/Extensions/VpnChanger/plugin.py-org
-wget http://vpnsetup.the-pm.club/openvpn/plugin.py -O /usr/lib/enigma2/python/Plugins/Extensions/VpnChanger/plugin.py
+wget https://raw.githubusercontent.com/thepmclub/VPNSetup/master/plugin.py -O /usr/lib/enigma2/python/Plugins/Extensions/VpnChanger/plugin.py
 chmod 777 /usr/lib/enigma2/python/Plugins/Extensions/VpnChanger/plugin.py
 opkg install openvpn
 rm /usr/sbin/pmcvpnsetup
-wget http://vpnsetup.the-pm.club/openvpn/pmcvpnsetup -O /usr/sbin/pmcvpnsetup
+wget https://raw.githubusercontent.com/thepmclub/VPNSetup/master/pmcvpnsetup -O /usr/sbin/pmcvpnsetup
 chmod 777 /usr/sbin/pmcvpnsetup
 echo "Success, everything is installed properly"
